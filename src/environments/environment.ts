a0008588f6bf6d47ecf8d0e7ecfc6bda4e751542
YUSUF HERI <32743573+yusufheri@users.noreply.github.com>
2019-05-04 22:17:22 +0100

    Update environment.ts
    Remove the google's keys.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 export const environment = {
   production: false,
   configFirebase : {
-    apiKey: 'AIzaSyDbMNyp8GeVRK0RGaCE1wapGZ4zLfUVNxM',
-    authDomain: 'customers-crud.firebaseapp.com',
-    databaseURL: 'https://customers-crud.firebaseio.com',
-    projectId: 'customers-crud',
-    storageBucket: 'customers-crud.appspot.com',
-    messagingSenderId: '264365068183'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
